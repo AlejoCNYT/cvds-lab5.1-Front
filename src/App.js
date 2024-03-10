dd9347795383ec0eb49fbe3554706927bab83ccd
Daniel Alejandro Acero Varela <89206637+AlejoCNYT@users.noreply.github.com>
2024-03-10 16:41:59 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from './logo.svg'; // Importa el logo de React
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World
         </p>
