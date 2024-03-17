0a0f74dcdaf87408272145ef43080afe3756b9a6
Daniel Alejandro Acero Varela <89206637+AlejoCNYT@users.noreply.github.com>
2024-03-17 13:22:47 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import logo from './logo.svg'; // Importa el logo de React
-import './App.css';
+import logo from './assets/images/logo.svg'; // Importa el logo de React
+import './assets/css/App.css';
+
+// Importar componentes
+import MiComponente from './components/MiComponente';
+
+
+function HelloWorld(names, undergraduate){
+  var presentation = <div>
+    <h2>Hello there, we're {names}</h2>
+    <h3>We're studying {undergraduate} </h3>
+  </div>
+
+
+  return presentation
+}
 
 function App() {
+
+  var names = "Daniel Alejandro Acero y Julian Triana"
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +27,14 @@ function App() {
         <p>
           Hello World
         </p>
+
+        { HelloWorld(names, "Computer Science") }
+
+      <section className='componentes'>
+        <MiComponente />
+
+        </section>
+
       </header>
     </div>
   );
